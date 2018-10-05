ebadc16a9179c421799116f70d934ff58e7b8f4b
johnvgomes <john.v.gomes@gmail.com>
2018-10-05 17:23:09 -0300

    first commit

---
+++ src/Model/TransictionsFunction.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author John Victor
+ */
+public class TransictionsFunction {
+    
+    public static String estadoInicial;
+    public static ArrayList<String> conjuntoEstadosFinais = new ArrayList<>();
+    public static ArrayList<String> conjuntoEstados = new ArrayList<>();
+    public static String [] AlfabetoEntrada ;
+    public static String [] AlfabetoDaFita ;
+
+       
+}
